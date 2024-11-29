39d6fb4722a80b75e03d8c699023dfd3c5e3a308
Michael Pfaff <michael@pfaff.dev>
2024-11-28 20:04:26 -0500

    Revert "Include fts5 extension"
    This reverts commit 95cbd414a7e96c7dd41b1c5f1188f53fc1c9f160.

--- src/lib.rs
+++ src/lib.rs
@@ -22,15 +22,11 @@ extern crate sqlite3_src;
 mod base;
 #[cfg(feature = "encryption")]
 mod encryption;
-#[cfg(feature = "ext-fts5")]
-mod ext_fts5;
 #[cfg(feature = "ext-spellfix")]
 mod ext_spellfix;
 
 pub use base::*;
 #[cfg(feature = "encryption")]
 pub use encryption::*;
-#[cfg(feature = "ext-fts5")]
-pub use ext_fts5::*;
 #[cfg(feature = "ext-spellfix")]
 pub use ext_spellfix::*;
